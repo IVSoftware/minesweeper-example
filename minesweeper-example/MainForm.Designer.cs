c04739f4274be8f7f3e499eb6d6ce158023fc7de
Tom Gregor <tgregor@ivsoftware.com>
2023-10-20 11:09:33 -0600

    Add project files.

---
+++ minesweeper-example/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace minesweeper_example
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 2;
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.Location = new System.Drawing.Point(55, 25);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 2;
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel.Size = new System.Drawing.Size(300, 150);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(578, 544);
+            Controls.Add(tableLayoutPanel);
+            Name = "MainForm";
+            Text = "Game Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+    }
+}
---
+++ minesweeper-example/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace minesweeper_example
+{
+    public partial class MainForm : Form
+    {
+        enum SelectedImage{ None, Flag, Bomb, }
+
+        Dictionary<SelectedImage, Image> Images = new Dictionary<SelectedImage, Image>
+        {
+            {  SelectedImage.None, null },
+            {  SelectedImage.Flag, Image.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "flag.png")) },
+            {  SelectedImage.Bomb, Image.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "bomb.png")) },
+        };
+        class MSButton : Button
+        {
+            public SelectedImage SelectedImage
+            {
+                get => _selectedImage;
+                set
+                {
+                    if (!Equals(_selectedImage, value))
+                    {
+                        _selectedImage = value;
+                        SelectedImageChanged?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            }
+            SelectedImage _selectedImage = 0;
+            public event EventHandler SelectedImageChanged;
+
+            // Cycle through the images
+            protected override void OnClick(EventArgs e)
+            {
+                base.OnClick(e);
+                SelectedImage = (SelectedImage)(((int)SelectedImage + 1) % 3);
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            const float DIM = 40F;
+            tableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel.RowCount = 10;
+            tableLayoutPanel.RowStyles.Clear();
+            tableLayoutPanel.ColumnCount = 10;
+            tableLayoutPanel.ColumnStyles.Clear();
+            for (int col = 0; col < 10; col++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, DIM));
+                for (int row = 0; row < 10; row++)
+                {
+                    tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, DIM));
+                    var button = new MSButton
+                    {
+                        Anchor = (AnchorStyles)0xF, // All
+                        BackgroundImageLayout = ImageLayout.Stretch,
+                    };
+                    button.SelectedImageChanged += (sender, e) =>
+                    {
+                        if(sender is MSButton msbutton)
+                        {
+                            msbutton.BackgroundImage = Images[msbutton.SelectedImage];
+                        }
+                    };
+                    tableLayoutPanel.Controls.Add(button, col, row);
+                }
+            }
+            // Size including 1 px borders.
+            tableLayoutPanel.Width = (int)((DIM * 10) + 11);
+            tableLayoutPanel.Height = (int)((DIM * 10) + 11);
+        }
+    }
+}
